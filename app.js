ba377d05f8200bb483fe79cba681667e48572a75
hdz1204 <shusheng1604@berkeley.com>
2020-12-04 03:40:16 -0800

    report

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var surveyRouter = require('./routes/survey');
+var searchRouter = require('./routes/searchResult');
 var reportRouter = require('./routes/report');
+var shareRouter = require('./routes/shareResults');
 
 var app = express();
 
@@ -22,7 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/survey',surveyRouter);
+app.use('/searchresult', searchRouter);
 app.use('/report', reportRouter);
+app.use('/share', shareRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
