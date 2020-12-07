7963c1e2fb550509cd76fb9fae57d73d5727f2d0
Siyi <casrom1994@gmail.com>
2020-12-07 15:56:09 -0500

    login required

--- public/js/login.js
+++ public/js/login.js
@@ -6,20 +6,21 @@ $(document).ready(()=>{
             $('#account-message').text("Already have an account? ");
             $('#sign-up-link').html("Log In");
             $('#login-button').html("Sign Up");
+            $('#login-form').attr('action', '/welcome')
         } else {
             $('#account-message').text("Don't have an account? ");
             $('#sign-up-link').html("Sign Up");
             $('#login-button').html("Log In");
+            $('#login-form').attr('action', '/home')
         }
         login = !login;
     });
 
-    $('#login-button').click((e)=>{
-        e.preventDefault();
-        if (login) {
-            window.location.href="/home";
-        } else {
-            window.location.href="/welcome";
-        }
-    })
+    // $('#login-form').click((e)=>{
+    //     if (login) {
+    //         window.location.href="/home";
+    //     } else {
+    //         window.location.href="/welcome";
+    //     }
+    // })
 })
