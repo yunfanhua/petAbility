9f3b5831d0766cd02c683f8c5def12baa662a4f7
Yunfan Hua <yunfanh@andrew.cmu.edu>
2020-12-08 18:21:54 -0800

    must make a choice for survey

--- public/js/survey.js
+++ public/js/survey.js
@@ -1,7 +1,9 @@
 const count = parseInt(window.location.pathname.split('/')[2])
+let chosen = false;
 $(document).on("click", ".choice-button", function() {
     $(".choice-button").removeClass("active");
     $(this).addClass("active");
+    chosen = true;
 });
 
 $(document).on("click", "#back-button", function() {
@@ -16,11 +18,16 @@ $(document).on("click", "#back-button", function() {
 });
 
 $(document).on("click", "#next-button", function() {
+    if (!chosen) {
+        alert("please make a choice");
+        return
+    }
     let newCount = count+1
     let url = "/survey/" + newCount;
     if (newCount === 6) {
         url = "/survey/result"
     }
     console.log(url);
+    chosen = false;
     window.location.replace(url);
 });
